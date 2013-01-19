26bcfa650ce6298abddf5042862d6a99bdf0ad72
Phil Maurer <philmau@gmail.com>
2013-01-19 19:26:01 +0000

    Adding more test tables

--- application/config/autoschema.php
+++ application/config/autoschema.php
@@ -6,8 +6,8 @@ AutoSchema::define('bills', function($table)
     $table->integer('user_id');
     $table->string('title');
     $table->string('name');
-    $table->text('comments');
-    $table->string('amount');
+    $table->text('comment');
+    $table->string('amounts');
     $table->string('recurrence'); // Weekly, Monthly or Yearly
     $table->integer('renews_on'); // Weekly = day of week, Monthly/Yearly = day of year
     $table->boolean('send_reminder'); // Should an email reminder be sent
@@ -19,8 +19,8 @@ AutoSchema::define('bills', function($table)
 AutoSchema::define('users', function($table)
 {
     $table->increments('id');
-    $table->string('forename', 255)->values('John');
-    $table->string('surnames', 255)->values('Smith');
+    $table->string('forename', 255);
+    $table->string('surname', 255);
     $table->string('username', 255);
     $table->integer('email')->values('emails:id,email');
     $table->string('communication_pref')->label('Communcation Preferences')->values(array('email'=>'Email', 'phone'=>'Phone', 'mail'=>'Mail'))->attributes(array('formtype'=>'checkbox'));
@@ -38,3 +38,84 @@ AutoSchema::define('emails', function($table)
     $table->boolean('active');
     $table->timestamps();
 });
+
+AutoSchema::define('bills2', function($table)
+{
+    $table->increments('id');
+    $table->integer('user_id');
+    $table->string('title');
+    $table->string('name');
+    $table->text('comments');
+    $table->string('amount');
+    $table->string('recurrence'); // Weekly, Monthly or Yearly
+    $table->integer('renews_on'); // Weekly = day of week, Monthly/Yearly = day of year
+    $table->boolean('send_reminder'); // Should an email reminder be sent
+    $table->integer('reminder'); // How many days before renewal to send reminder
+    $table->boolean('include_in_totals');
+    $table->timestamps();
+});
+
+AutoSchema::define('bills3', function($table)
+{
+    $table->increments('id');
+    $table->integer('user_id');
+    $table->string('title');
+    $table->string('name');
+    $table->text('comments');
+    $table->string('amount');
+    $table->string('recurrence'); // Weekly, Monthly or Yearly
+    $table->integer('renews_on'); // Weekly = day of week, Monthly/Yearly = day of year
+    $table->boolean('send_reminder'); // Should an email reminder be sent
+    $table->integer('reminder'); // How many days before renewal to send reminder
+    $table->boolean('include_in_totals');
+    $table->timestamps();
+});
+
+for($i=0; $i<60; $i++){
+    AutoSchema::define('bills'.$i, function($table)
+    {
+        $table->increments('id');
+        $table->integer('user_id');
+        $table->string('title');
+        $table->string('name');
+        $table->text('comments');
+        $table->string('amount');
+        $table->string('recurrence'); // Weekly, Monthly or Yearly
+        $table->integer('renews_on'); // Weekly = day of week, Monthly/Yearly = day of year
+        $table->boolean('send_reminder'); // Should an email reminder be sent
+        $table->integer('reminder'); // How many days before renewal to send reminder
+        $table->boolean('include_in_totals');
+        $table->string('col1');
+        $table->string('col2');
+        $table->string('col3');
+        $table->string('col4');
+        $table->string('col5');
+        $table->string('col6');
+        $table->string('col7');
+        $table->string('col8');
+        $table->string('col9');
+        $table->string('col10');
+        $table->string('col11');
+        $table->string('col12');
+        $table->string('col13');
+        $table->string('col14');
+        $table->string('col15');
+        $table->string('col16');
+        $table->string('col17');
+        $table->string('col18');
+        $table->string('col19');
+        $table->string('col20');
+        $table->string('col21');
+        $table->string('col22');
+        $table->string('col23');
+        $table->string('col24');
+        $table->string('col25');
+        $table->string('col26');
+        $table->string('col27');
+        $table->string('col28');
+        $table->string('col29');
+        $table->string('col30');
+        $table->string('col31');
+        $table->timestamps();
+    });
+}
