d30c3c1fde84259d7a4664ca3241bda5bda3cbf1
Phil Maurer <philmau@gmail.com>
2013-02-23 00:46:32 +0000

    Changing the driver interface into a class as it's more useful that way

--- application/libraries/autoschema/drivers/driver.php
+++ application/libraries/autoschema/drivers/driver.php
@@ -2,26 +2,6 @@
 
 use \AutoSchema\AutoSchema;
 
-interface Driver{
+class Driver{
 	
-	public static function create_table( $table );
-
-	public static function create_view( $view );
-
-	public static function drop_table( $table );
-
-	public static function drop_view( $view );
-
-	public static function column_definition( $column=array() );
-
-	public static function tables_in_database( );
-
-	public static function views_in_database( );
-
-	public static function columns_in_table( $table );
-
-	public static function update_table( $table );
-
-	public static function update_view( $view );
-
 }
